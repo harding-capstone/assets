5a70963ba46b0505444867e49270c4109d9a8a80
Trevor Hale <thale4@harding.edu>
2019-04-11 00:48:15 -0500

    Map files added.

---
+++ maps/BigMapDesert/ice.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="ice" tilewidth="16" tileheight="16" tilecount="640" columns="20">
+ <image source="pixel_animations_gfxpack/animationsheets/ice.png" width="320" height="512"/>
+</tileset>
---
+++ maps/BigMapDesert/tf_winter_water.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="tf_winter_water" tilewidth="16" tileheight="16" tilecount="682" columns="22">
+ <image source="TimeFantasy_Winter/tiles/tf_winter_water.png" width="352" height="496"/>
+</tileset>
