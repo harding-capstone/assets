0081be07dbdc753faf6777668287ca93a7a648d8
Trevor Hale <thale4@harding.edu>
2019-04-11 00:50:19 -0500

    Changed folder name containing map files.

--- maps/BigMapDesert/castle.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.2" name="castle" tilewidth="16" tileheight="16" tilecount="578" columns="34">
- <image source="TILESETS/castle.png" trans="ff00ff" width="544" height="272"/>
-</tileset>
--- maps/BigMapDesert/water.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.2" name="water" tilewidth="16" tileheight="16" tilecount="2805" columns="51">
- <image source="TILESETS/water.png" trans="ff00ff" width="816" height="880"/>
-</tileset>
